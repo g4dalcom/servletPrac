7fb295fe6d764e37a309f917b110189c05495d8e
g4dalcom <g4dalcom@gmail.com>
2022-09-26 00:06:27 +0900

    basic sevlet

--- src/main/java/com/study/servlet/basic/HelloSevlet.java
+++ src/main/java/com/study/servlet/basic/HelloSevlet.java
@@ -1,2 +1,30 @@
-package com.study.servlet.basic;public class HelloSevlet {
+package com.study.servlet.basic;
+
+import javax.lang.model.SourceVersion;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "helloSevlet", urlPatterns = "/hello")
+public class HelloSevlet extends HttpServlet {
+
+    // Ctrl + O 누른 후 자물쇠 아이콘 service
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloSevlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
 }
