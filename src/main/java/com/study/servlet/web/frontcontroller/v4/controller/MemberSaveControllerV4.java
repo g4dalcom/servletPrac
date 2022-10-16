fe763ba8ce40cea9348397497047fcc696b8bab7
g4dalcom <g4dalcom@gmail.com>
2022-10-16 23:56:48 +0900

    controller v4

--- src/main/java/com/study/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
+++ src/main/java/com/study/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -2,27 +2,23 @@ package com.study.servlet.web.frontcontroller.v3.controller;
 
 import com.study.servlet.domain.member.Member;
 import com.study.servlet.domain.member.MemberRepository;
-import com.study.servlet.web.frontcontroller.ModelView;
-import com.study.servlet.web.frontcontroller.v3.ControllerV3;
-import com.study.servlet.web.frontcontroller.v3.ControllerV4;
+import com.study.servlet.web.frontcontroller.v4.ControllerV4;
 
 import java.util.Map;
 
-public class MemberSaveControllerV3 implements ControllerV3 {
+public class MemberSaveControllerV4 implements ControllerV4 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    public ModelView process(Map<String, String> paramMap) {
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
         String username = paramMap.get("username");
         int age = Integer.parseInt(paramMap.get("age"));
 
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        ModelView mv = new ModelView(("save-result"));
-        mv.getModel().put("member", member);
-
-        return mv;
+        model.put("member", member);
+        return "save-result";
     }
 }
